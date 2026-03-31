3220fa1e41522731880404ae8b652fe00f91e6c0
Vovk Snizhana <snizhana.vovk.pp.2023@lpnu.ua>
2026-03-31 13:28:01 +0300

    feat: add CI/CD pipeline

--- react-project/eslint.config.js
+++ react-project/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-hooks/no-direct-set-state-in-use-effect': 'off',
+      'react-refresh/only-export-components': 'off',
     },
   },
   {
